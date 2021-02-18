f366eac520abc4d2ea82a945cacd0b1ede701935
dave@solarbotics.com <H4hOKPr89aBS4xorIjOH>
2021-02-18 00:31:06 -0700

    Got the scale reporting properly

--- serial_scale.py
+++ serial_scale.py
@@ -1,3 +1,5 @@
+#From https://github.com/zmitchell/async-serial/blob/master/async_serial_protocol.py
+
 import asyncio
 import serial_asyncio
 
@@ -12,16 +14,26 @@ class Reader(asyncio.Protocol):
         print('Reader connection created')
 
     def data_received(self, data):
-        """Store characters until a newline is received.
+        """Store characters until a 'kg' is received.
         """
         self.buf += data
-        if b'\n' in self.buf:
+        if b'kg,\r' in self.buf:
+            # print(self.buf)
+            if b'Readings:' in self.buf: # Reset the buffer to zero for the post-boot message filtering
+                self.buf = b'0.000,kg\r\n'
+                print('gotcha')
+
             lines = self.buf.split(b'\n')
+            #lines = self.buf.split(b',')
             self.buf = lines[-1]  # whatever was left over
-            for line in lines[:-1]:
-                print(f'Reader received: {line.decode()}')
-                self.msgs_recvd += 1
-        if self.msgs_recvd == 15:
+
+            # print('selfbuf:',self.buf)
+            # for line in lines[:-1]:
+            #     print(f'Reader received: {line.decode()}')
+            #     self.msgs_recvd += 1
+            print('Lines:',lines)
+            print("Reader:",lines[0].decode('ascii').split(',')[0])
+        if self.msgs_recvd == 8:
             self.transport.close()
 
     def connection_lost(self, exc):
@@ -55,10 +67,10 @@ loop = asyncio.get_event_loop()
 # reader = serial_asyncio.create_serial_connection(loop, Reader, 'reader', baudrate=9600)
 # writer = serial_asyncio.create_serial_connection(loop, Writer, 'writer', baudrate=9600)
 reader = serial_asyncio.create_serial_connection(loop, Reader, '/dev/ttyUSB0', baudrate=9600)
-writer = serial_asyncio.create_serial_connection(loop, Writer, '/dev/ttyUSB0', baudrate=9600)
+# writer = serial_asyncio.create_serial_connection(loop, Writer, '/dev/ttyUSB0', baudrate=9600)
 asyncio.ensure_future(reader)
 print('Reader scheduled')
-asyncio.ensure_future(writer)
+# asyncio.ensure_future(writer)
 print('Writer scheduled')
 loop.call_later(10, loop.stop)
 loop.run_forever()
