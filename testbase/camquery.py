978b116c03f5f13a5aebcc29c6a2704fc3e1bd2d
dave@solarbotics.com <H4hOKPr89aBS4xorIjOH>
2021-04-13 21:44:48 -0600

    Starting to kill redlines

--- testbase/camquery.py
+++ testbase/camquery.py
@@ -3,7 +3,12 @@
 # ref:  lookup_sku - Polls database for required SKU/Item data
 # in the __main__ function
 
- Poll database for lookup of scanned SKU
+# Poll database for lookup of scanned SKU
+import logging
+from tornado import httpclient, escape
+from camstationv2 import BroadcastHandler
+
+
 async def lookup_sku(sku, path=None):
 	sku = sku.strip()
 	if not sku:
@@ -18,6 +23,7 @@ async def lookup_sku(sku, path=None):
 
 	client = httpclient.AsyncHTTPClient()
 	try:
+		from tornado import httputil
 		url = httputil.url_concat('http://192.168.2.8:9002/lookup.json', {'sku': sku})
 		response = await client.fetch(url)
 	except Exception as ex:
@@ -26,6 +32,6 @@ async def lookup_sku(sku, path=None):
 	else:
 		# TODO: {KL} Include any images already attached to the sku
 		result['item'] = escape.json_decode(response.body)
-		result['item']['images'] = list(enumerate_images(path, result['item']['active_bag']))
+		# result['item']['images'] = list(enumerate_images(path, result['item']['active_bag']))
 
 	await BroadcastHandler.event('lookup', result)
